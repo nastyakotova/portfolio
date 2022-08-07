d888db1eef9fc629f825666cbe288c93e2f3b39a
Anastasia <nastya-kotikm@yandex.ru>
2022-08-07 22:57:10 +0300

    Реализован роутинг

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
-import styles from './App.module.scss';
-import Navigation from '../Navigation/Navigation';
-import About from '../About/About';
+// import styles from './App.module.scss';
+import { Provider } from "react-redux";
+import { Routes, Route } from 'react-router-dom';
+import store from "../../store";
+import Main from "../Main/Main";
+import About from "../About/About";
+import Portfolio from "../Portfolio/Portfolio";
 
 export default function App() {
   return (
-    <div>
-      <Navigation />
-      <About />
-    </div>
+    <Provider store={store}>
+      <Routes>
+          <Route path="/" element={<Main />}>
+            <Route index element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Route>
+        </Routes>
+    </Provider>
   );
 }
