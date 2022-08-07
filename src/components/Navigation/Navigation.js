f6104a5e5c641bdc8d2dc981669238c2305d3135
Anastasia <nastya-kotikm@yandex.ru>
2022-08-07 22:59:40 +0300

    Изменена навигация

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,30 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { activatePopup, getContentType } from "../../store/popup/actionsCreators";
 import styles from './Navigation.module.scss';
 
 export default function Navigation() {
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.blur}>
       <nav className={styles.navigation}>
         <div>
           <h1>Anastasia.</h1>
-          <p>Frontend Developer</p>
+          <p>Frontend Разработчик</p>
         </div>
         <div>
-          <a href="#">Ссылка</a>
-          <a href="#">Ссылка</a>
-          <a href="#">Ссылка</a>
-          <a href="#">Ссылка</a>
+          <Link to="/">Обо мне</Link>
+          <Link to="/portfolio">Портфолио</Link>
+          <a
+            href="/"
+            onClick={(event) => {
+              event.preventDefault();
+              dispatch(getContentType('contacts'));
+              dispatch(activatePopup());
+            }}
+          >Контакты</a>
         </div>
       </nav>
     </div>
